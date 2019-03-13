ab03bd7fb012edae78c6bea5fbece5e8eee48562
Daniel La <la.danieln@gmail.com>
2019-03-13 13:18:17 -0400

    Installed and added bodyparser for handling post requests

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-let express = require('express');
+let express = require('express'),
+    toDoRoutes = require('./routes/todo'),
+    bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello from the root route!');
 });
 
+// Set the route for todos and use the router in todo to handle
+app.use('/api/todo', toDoRoutes);
 
 
 let port = process.env.PORT || 3000;
